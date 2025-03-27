6ab653d82d577c7ec38fd5affd3cdfd1c2bc2e6d
Arijit-nayak <nayakarijit8@gmail.com>
2025-03-27 23:30:10 +0530

    login page added and connection made

---
+++ src/EMS/Main_class.java
@@ -0,0 +1,16 @@
+package EMS;
+
+import javax.swing.*;
+
+public class Main_class extends JFrame {
+
+    Main_class() {
+        setSize(1120, 630);
+        setLocation(250, 100);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Main_class();
+    }
+}
--- src/EMS/Splash.java
+++ src/EMS/Splash.java
@@ -21,7 +21,7 @@ public class Splash extends JFrame {
         try {
             Thread.sleep(4000);
             setVisible(false);
-
+            new Login();
         } catch (Exception e) {
             e.printStackTrace();
         }
